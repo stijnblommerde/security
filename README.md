0006d3932b9f30ac7d34d826d5ce149077e6ad41
stijnblommerde <stijnblommerde@tessas-MacBook-Pro-2.local>
2017-02-26 20:05:37 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+security
+========
+
+A Symfony project created on February 26, 2017, 5:10 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function adminAction()
+    {
+        // replace this example code with whatever you need
+        return $this->render(':default:index.html.twig');
+    }
+}
